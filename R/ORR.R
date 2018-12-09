41058347e45d0b8ae95f68440fd33a31da66b77e
Jgilman1 <45524190+Jgilman1@users.noreply.github.com>
2018-12-09 20:11:33 +0000

    Update ORR.R

--- R/ORR.R
+++ R/ORR.R
@@ -2,7 +2,7 @@
 ##' 
 ##' @title Objective Response Rate and Binomial Confidence interval
 ##' @param x a number indicating total number of subjects with Partial Response or Complete Response
-##' @param n a number indicating total number of subjects
+##' @param n a number indicating total number of evaluable subjects
 ##' @return Objective Response Rate and 95% binomial confidence interval
 ##' @author Jon
 ##' @export
